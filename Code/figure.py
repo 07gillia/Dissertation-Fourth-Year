e6c5e5d713c68f6e14fd842634a1537d9362a5ba
colossusMini <07gillia@googlemail.com>
2018-04-20 21:27:25 +0100

    basically finished poster, and made some more figures for the paper that haven't been applied

--- Code/figure.py
+++ Code/figure.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import sys
 
 dataframe =  pd.read_csv('../Data/ALLSTOCKS.csv')
 
@@ -10,18 +12,65 @@ dataframe['DATETIME'] = pd.to_datetime(dataframe['DATE'] + ' ' + dataframe['TIME
 
 dataframe = dataframe.sort(['DATETIME'])
 
-# get the last 22 datapoints
-for x in range(30, len(dataframe['DATETIME'])):
+def Bolinger_Bands(index):
 
-	start = x - 22
+	start = index - 22
 
-	data = dataframe.iloc[22, start:x]
-	print(data)
+	data = dataframe.iloc[start:index,25:26]
+	# the one is the stock index
 
-print(dataframe)
+	average = np.mean(data)
+
+	sd = np.std(data)
+
+	middle_band = float(average)
+	upper_band = float(average + (sd * 2))
+	lower_band = float(average - (sd * 2))
+
+	return middle_band, upper_band, lower_band
+
+result_middle = []
+result_upper = []
+result_lower = []
+
+a = float('nan')
+
+for x in range(1,31):
+	result_middle.append(a)
+	result_upper.append(a)
+	result_lower.append(a)
+
+for x in range(30,len(dataframe['DATETIME'])):
+
+	stock_price = dataframe.iloc[x, 25]
+
+	if np.isnan(stock_price):
+		result_middle.append(a)
+		result_upper.append(a)
+		result_lower.append(a)
+	else:
+
+		result = Bolinger_Bands(x)
+
+		result_middle.append(result[0])
+		result_upper.append(result[1])
+		result_lower.append(result[2])
+
+dataframe['middle'] = result_middle
+dataframe['upper'] = result_upper
+dataframe['lower'] = result_lower
+
+# dataframe = dataframe.tail(390)
+
+# print(dataframe)
 
 f = plt.figure()
 plt.plot(dataframe['DATETIME'], dataframe['HD'])
+plt.plot(dataframe['DATETIME'], dataframe['middle'])
+plt.plot(dataframe['DATETIME'], dataframe['upper'])
+plt.plot(dataframe['DATETIME'], dataframe['lower'])
+
+plt.legend(['Stock Price', 'Middle Band', 'Upper Band', 'Lower Band'], loc='upper left')
 f.suptitle('HD Stock Price')
 plt.xlabel('Time')
 plt.ylabel('Stock Price (£)')
@@ -29,4 +78,5 @@ plt.show()
 
 f.savefig("figure.pdf", bbox_inches='tight')
 
+#						4		5		6		7		8		9		10	11		12	13		14		15		16	17		18
 # Stock_Names = c("AA", "AAPL", "ADBE", "AIG", "AMAT", "AMT", "AXP", "BA", "BAC", "C", "CA", "CAT", "CSCO", "CVX", "DD", "DIS", "EMC", "FSLR", "GE", "GLW", "GOOG", "GS", "HD", "HPQ", "IBM", "INTC", "IP", "JNJ", "JPM", "KO", "MCD", "MMM", "MRK", "MSFT", "PFE", "PG", "T", "TRV", "UTX", "VZ", "WFC", "WMT", "XOM", "YAHOO", "YNDX")
---
+++ Code/figure2.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+dataframe =  pd.read_csv('../Data/ALLSTOCKS.csv')
+
+pd.to_datetime(dataframe['DATE'])
+pd.to_datetime(dataframe['TIME'])
+
+dataframe['DATETIME'] = pd.to_datetime(dataframe['DATE'] + ' ' + dataframe['TIME'])
+
+dataframe = dataframe.sort(['DATETIME'])
+
+# dataframe = dataframe.tail(390)
+
+a = float('nan')
+
+peaks = []
+troughs = []
+
+for x in range(1,5001):
+	peaks.append(a)
+	troughs.append(a)
+
+for x in range(5000,(len(dataframe['DATETIME'])-1)):
+
+	stock_price = float(dataframe.iloc[x, 11])
+	previous_stock_price = float(dataframe.iloc[(x-1),11])
+	next_stock_price = float(dataframe.iloc[(x+1),11])
+
+	start = x - 2490
+	data = dataframe.iloc[start:x,11]
+
+	average_price = float(np.mean(data))
+
+	# print(average_price)
+
+	# print(stock_price)
+	# print(previous_stock_price)
+	# print(next_stock_price)
+
+	if (stock_price > previous_stock_price) & (stock_price > next_stock_price) & (stock_price > average_price):
+		peaks.append(stock_price)
+		troughs.append(a)
+	elif (stock_price < previous_stock_price) & (stock_price < next_stock_price) & (stock_price < average_price):
+		peaks.append(a)
+		troughs.append(stock_price)
+	else:
+		peaks.append(a)
+		troughs.append(a)
+
+	# print(peaks)
+	# print(troughs)
+
+peaks.append(a)
+troughs.append(a)
+
+print(len(peaks))
+print(len(troughs))
+print(len(dataframe['DATETIME']))
+
+dataframe['peaks'] = peaks
+dataframe['troughs'] = troughs
+
+DATETIMES = dataframe['DATETIME'].tolist()
+
+# print(dataframe)
+
+plt.scatter(DATETIMES, peaks, color='b')
+plt.scatter(DATETIMES, troughs, color='r')
+
+plt.legend(['Sell Points', 'Buy Points'], loc='upper left')
+
+plt.suptitle('BAC SVM Input Classes')
+plt.xlabel('Time')
+plt.ylabel('Stock Price (£)')
+plt.savefig("figure.pdf", bbox_inches='tight')
+plt.show()
+
+#						4		5		6		7		8		9		10	11		12	13		14		15		16	17		18
+# Stock_Names = c("AA", "AAPL", "ADBE", "AIG", "AMAT", "AMT", "AXP", "BA", "BAC", "C", "CA", "CAT", "CSCO", "CVX", "DD", "DIS", "EMC", "FSLR", "GE", "GLW", "GOOG", "GS", "HD", "HPQ", "IBM", "INTC", "IP", "JNJ", "JPM", "KO", "MCD", "MMM", "MRK", "MSFT", "PFE", "PG", "T", "TRV", "UTX", "VZ", "WFC", "WMT", "XOM", "YAHOO", "YNDX")
